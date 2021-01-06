07e310a324987407a95c9f87bdcfa8880ca0b061
Jenny Tang <Jentang1996@gmail.com>
2021-01-05 19:09:43 -0500

    Update Beginner_Level_SQL.sql

--- Beginner_Level_SQL.sql
+++ Beginner_Level_SQL.sql
@@ -43,3 +43,49 @@ select
 from Orders 
 where EmployeeID = 5
 
+/*Question 6: Suppliers table show the SupplierID, ContactName, and ContactTitle for the Suppliers whose ContactTitle is NOT marketing Manager */
+
+select
+	SupplierID,
+	ContactName,
+	ContactTitle
+from Suppliers 
+where ContactTitle != 'Marketing Manager'
+
+/*Question 7: In the products table select ProductID and ProductName for those products where the ProductName includes the string "queso"*/
+
+select
+	ProductID,
+	ProductName
+from Products
+where ProductName like '%queso%'
+
+/*Question 8: Using Orders table write a query that shows the OrderID, CustomerID and ShipCountry for all orders where teh ShipCountry is either France or Belguimm */
+
+select 
+	OrderID,
+	CustomerID,
+	ShipCountry
+from Orders 
+where ShipCountry = 'France' or ShipCountry = 'Belgium'
+
+/*Question 9: Using Orders Table write a query to return all orders from any Latin American Country. (Brazil, Maxico, Argentina, Venezuela) */
+
+select 
+	OrderID,
+	CustomerID,
+	ShipCountry
+from Orders 
+where ShipCountry in ('Brazil','Mexico','Argentina','Venezuela')
+
+/*Question 10: For all Employees in the Employee table show the FirstName, LastName, Title and BirthDate. Order the results by Birthdate so we have the oldest employees first */
+
+select
+	FirstName,
+	LastName,
+	Title,
+	BirthDate
+from Employees
+order by BirthDate Asc 
+
+
