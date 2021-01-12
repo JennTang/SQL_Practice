6b4b4a092b402d89a8fe541f0d9eae5e9b0d4ff8
Jenny Tang <Jentang1996@gmail.com>
2021-01-11 20:58:34 -0500

    Create Intermediate_Level_SQL.sql

---
+++ Intermediate_Level_SQL.sql
@@ -0,0 +1,9 @@
+/***********************************************************/
+/*                                                         */
+/* The following SQL Problems are from the book            */
+/*     SQL Practice Problems by Sylvia Moestl Vasilik      */
+/* The .sql file will contain Intermediate Problems        */
+/*                                                         */
+/***********************************************************/
+
+/* Question 1: Return all fields from the Shipper Table *
